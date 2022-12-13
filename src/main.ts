5c2cf4bc72d614a08d932f21ccb61046908e3182
So <im.lfq@139.com>
2022-12-14 00:00:14 +0800

    feat: 界面美化完成

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,39 @@
-import { XHR } from './core/XHR';
-import './style.scss';
+import { $ } from "./core/DOM";
+import { XHR } from "./core/XHR";
+import "./style.scss";
 
 // 微信跳转登录页
 if (location.pathname == "/" && location.search.indexOf("target=t") == -1) {
-    location.search = location.search == "" ? "?target=t" : "&target=t"
+  location.search = location.search == "" ? "?target=t" : "&target=t";
 }
 
 // 微信登录使用 UOS 密钥强登
-const extspam = import.meta.env.VITE_EXTSPAM
+const extspam = import.meta.env.VITE_EXTSPAM;
 XHR.addListener(XHR.ReadyState.OPENED, (xhr, uri) => {
-    if (uri.pathname == "/cgi-bin/mmwebwx-bin/webwxnewloginpage") {
-        xhr.setRequestHeader("extspam", extspam)
-        xhr.setRequestHeader("client-version", "2.0.0")
-    }
-})
+  if (uri.pathname == "/cgi-bin/mmwebwx-bin/webwxnewloginpage") {
+    xhr.setRequestHeader("extspam", extspam);
+    xhr.setRequestHeader("client-version", "2.0.0");
+  }
+});
+
+document.addEventListener("load", () => {
+  console.log(11111);
+});
+
+const contextMenu = $("#contextMenu")
+if (contextMenu) {
+  $(".main_inner")?.insertAdjacentElement('beforebegin', contextMenu)
+}
+
+const tab = $(".panel .tab");
+if (tab) {
+  $(".panel .header .info")?.insertAdjacentElement('beforebegin', tab)
+}
+
+const header = $(".panel .header");
+if (header) {
+  console.log(header);
+  $(".main_inner")?.insertAdjacentElement('afterbegin', header)
+}
+
+
